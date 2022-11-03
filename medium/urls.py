dab76fb33f8c383a268c964da915f69aee54caae
steffyjk <steffy.jk2018@gmail.com>
2022-11-03 18:13:53 +0530

    User registration & login functionality

--- medium/urls.py
+++ medium/urls.py
@@ -16,8 +16,9 @@ router.register(r'image', views.ImageViewSet, basename='Image')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/', include('auth.urls')),
     path('', include(router.urls)),
 ]
 
